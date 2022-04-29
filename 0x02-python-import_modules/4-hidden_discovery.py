9d0545ebf31f83b7f80eb67c132cbed305084ce3
Achref-haddaji <haddaji.achref19@gmail.com>
2022-04-29 11:01:34 +0100

    task4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import  hidden_4
-for a in dir(hidden_4):
-	if a[0] != '_':
-		print(a)
+if __name__ == "__main__":
+	import hidden_4
+	for a in dir(hidden_4):
+		if a[0] != '_':
+			print(a)
