f4a33eb5b50861216e57973cb071a17406f8a894
Achref-haddaji <haddaji.achref19@gmail.com>
2022-06-06 15:04:50 +0100

    rectangl

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Rectangle module """
-from models.base import Base
+from . base import Base
 
 
 class Rectangle(Base):
@@ -133,7 +133,7 @@ class Rectangle(Base):
                     if i == "y":
                         self.y = kwargs["y"]
 
-	def to_dictionary(self):
+    def to_dictionary(self):
         """
         returns the dictionary representation of a Rectangle
         """
