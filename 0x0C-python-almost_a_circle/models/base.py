1eea5fe420d35cb8fefaba60478724462c1d4653
Achref-haddaji <haddaji.achref19@gmail.com>
2022-06-06 14:42:22 +0100

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,3 +29,42 @@ class Base:
             return ("[]")
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        writes the JSON string representation of list_objs to a file
+        """
+        list = []
+        if list_objs is None:
+            list = []
+        else:
+            for i in list_objs:
+                list.append(cls.to_dictionary(i))
+        with open("{}.json".format(cls.__name__), 'w', encoding="UTF8") as f:
+            f.write(cls.to_json_string(list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        returns the list of the JSON string representation json_string
+        """
+        list = []
+        if json_string is None or json_string == "":
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set.
+        Args:
+            - dictionary: used as **kwargs
+        Returns: instance created
+        """
+        if cls.__name__ == 'Rectangle':
+            dummy = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
