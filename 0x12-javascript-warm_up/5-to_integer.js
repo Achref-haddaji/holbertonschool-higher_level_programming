789f8f4bc77502d8b77c924f28e24b775d93104f
Achref-haddaji <haddaji.achref19@gmail.com>
2022-08-01 15:07:13 +0100

    task5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const process = require('process');
-if (isNaN(process.argv[2])) {
-	   console.log('Not a number');
-} else {
-  console.log('My mumber : ' + parseInt(process.argv[2]));
-}
+if (isNaN(Number(process.argv[2]))) {
+	console.log('Not a number');
+  } else {
+	console.log('My number: ' + Number(process.argv[2]));
+  }
