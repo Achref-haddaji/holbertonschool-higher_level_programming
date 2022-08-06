a7a601fecf83fc32c499ad06ce663a70a11fc1a6
Achref-haddaji <haddaji.achref19@gmail.com>
2022-08-06 21:38:16 +0100

    task2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,37 @@
 #!/usr/bin/python3
-""" states module"""
+"""
+states module
+"""
+
+
 import MySQLdb
-from sys import argv
+import sys
+
+
+def myfilter_states():
+    """"
+    lists all states with a name starting with N (upper N)
+    from the database hbtn_0e_0_usa
+    """
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    searchstate = sys.argv[4]
+    cities = []
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=user,
+                         passwd=password, db=database)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states\
+                WHERE name='{}'\
+                ORDER BY id ASC".format(searchstate))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
 
 if __name__ == "__main__":
-    """ connect to the database"""
-    db = MySQLdb.connect(
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
-    )
-    """ i didnt assgin the *host value and the *port value in the arguments
-    cuz they will be ther bye default """
-cursor = db.cursor()
-cursor.execute("SELECT * FROM states".format(argv[4]))
-result = cursor.fetchall()
-for i in result:
-    if i[1] == argv[4]:
-        """ print the once that starts with *N """
-        print(i)
-cursor.close()
-""" close the cursor"""
-db.close()
-""" close the database"""
+    myfilter_states()
