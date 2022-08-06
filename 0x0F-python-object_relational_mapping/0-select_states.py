e308dca75c9eb27850b518ac71059f7d0b6773d9
Achref-haddaji <haddaji.achref19@gmail.com>
2022-08-06 12:06:56 +0100

    task0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-""" states module """
-import MySQLdb
-from sys import argv
-
+"""
+Lists all states from the database hbtn_0e_0_usa.
+* Usage: ./0-select_states.py <mysql username> \
+*                            <mysql password> \
+*                             <database name>
+"""
 if __name__ == "__main__":
-    """connect to the database"""
-    db = MySQLdb.connect(
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
-    )
-    """ i didnt declare the *host and the *port arguments
-    cuz they will be ther bye default """
-cursor = db.cursor()
-cursor.execute("SELECT * FROM states ORDER BY id")
-result = cursor.fetchall()
-for i in result:
-    """ loop the states"""
-    print(i)
-cursor.close()
-""" close the cursor"""
-db.close()
-""" close the database"""
+    import MySQLdb
+    import sys
+
+    uname = sys.argv[1]
+    upass = sys.argv[2]
+    dbname = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", port=3306, user=uname,
+                         passwd=upass, db=dbname)
+    c = db.cursor()
+    c.execute("""SELECT * from states ORDER BY states.id ASC""")
+
+    line = c.fetchone()
+    while (line):
+        print(line)
+        line = c.fetchone()
