f706889f86ff9c4a0d4ed11b100a12c5a8042d95
Achref-haddaji <haddaji.achref19@gmail.com>
2022-09-14 11:20:11 +0100

    task6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const todos = JSON.parse(body);
+    const completed = {};
+    for (const x of todos) {
+      if (x.completed === true) {
+        if (x.userId in completed) { completed[x.userId]++; } else { completed[x.userId] = 1; }
+      }
+    }
+    console.log(completed);
+  }
+});
